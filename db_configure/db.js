3935b0c486e7f9f69da1bf042cad564ab01bca89
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-12-09 21:47:42 +0530

    updated

--- db_configure/db.js
+++ db_configure/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const uri="mongodb+srv://ranjithbaswaraju10_db_user:8RORkTMzLw1dQ4g9@sampletodo.ciin6sz.mongodb.net/"
 
 async function connectDB() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/", {
+    await mongoose.connect(url, {
       dbName: "MVC_Todo",
     });
     console.log("Database Connected");
